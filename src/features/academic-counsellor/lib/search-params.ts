2d8e9e9ac3ed7cd4cf55b6e7c359efa26f8f2189
Prabesh Shrestha <prabeshshrestha@Prabeshs-MacBook-Air.local>
2026-06-28 14:06:14 +0545

    chore(academic-counsellor): remove obsolete search-params
    Three-line wrapper around parseAcademicCounsellorSearchParams; folded
    into lib/url-state.ts as loadAcademicCounsellorState.

--- src/features/academic-counsellor/lib/search-params.ts
+++
@@ -1,8 +0,0 @@
-import { parseAcademicCounsellorSearchParams, type AcademicCounsellorSearchParams } from './filter-url-state'
-
-export async function loadAcademicCounsellorState(
-  searchParams: Promise<AcademicCounsellorSearchParams>
-) {
-  const { filters, page } = parseAcademicCounsellorSearchParams(await searchParams)
-  return { filters, page }
-}
